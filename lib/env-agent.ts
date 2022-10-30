867aa107558dc4d0117691dc922e783e616523b8
Michael <me@miikey.pw>
2022-10-31 10:27:14 +1100

    feat(env-agent): added internal documentation to available methods

--- lib/env-agent.ts
+++ lib/env-agent.ts
@@ -12,12 +12,25 @@ interface EnvType {
 }
 
 interface EnvManipulator {
+    /**
+     * Parses a .env file and returns an object containing the environment variables.
+     */
     parse(file: Buffer): EnvType;
 
+    /**
+     * Configures the environment variables by reading the .env file and sets the variable
+     * if it does not exist.
+     */
     configure(): EnvType;
 
+    /**
+     * Programmatically set environment variables that are only available in the current process.
+     */
     set(key: string, value: string): void;
 
+    /**
+     * Retrieve an environment variable from the current process.
+     */
     get(key?: string): string;
 }
 
