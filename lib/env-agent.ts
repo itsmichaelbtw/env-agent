5d28a57a453ba226adf759c9191876a7bb1ad7ac
Michael <me@miikey.pw>
2022-10-30 22:32:09 +1100

    feat(env-agent): configure, parse and get environment variables

---
+++ lib/env-agent.ts
@@ -0,0 +1,102 @@
+import path from "path";
+import fs from "fs";
+import os from "os";
+
+import { isUndefined, hasOwnProperty } from "./utils";
+
+const DOTENV_FILENAME = ".env";
+const DOTENV_LINE = /^\s*([^\#]+)\s*=\s*([^#]*)/m;
+
+interface EnvType {
+    [key: string]: string;
+}
+
+interface EnvManipulator {
+    parse(file: Buffer): EnvType;
+
+    configure(): EnvType;
+
+    set(key: string, value: string): void;
+
+    get(key?: string): string;
+}
+
+function parse(file: Buffer): EnvType {
+    try {
+        const environmentVariables: EnvType = {};
+        const fileAsString = file.toString();
+        const lines = fileAsString.split(os.EOL);
+
+        for (const potentialLine of lines) {
+            const isMatch = DOTENV_LINE.test(potentialLine);
+
+            if (!isMatch) {
+                continue;
+            }
+
+            const line = potentialLine.match(DOTENV_LINE);
+
+            if (line === null) {
+                continue;
+            }
+
+            const keyValuePairs = line.slice(1);
+
+            let key = keyValuePairs[0] ?? "";
+            let value = keyValuePairs[1] ?? "";
+
+            key = key.trim();
+            value = value.trim();
+
+            environmentVariables[key] = value;
+        }
+
+        return environmentVariables;
+    } catch (error) {
+        return {};
+    }
+}
+
+function configure(): EnvType {
+    const envPath = path.resolve(process.cwd(), DOTENV_FILENAME);
+
+    try {
+        const file = fs.readFileSync(envPath);
+        const env = parse(file);
+
+        for (const key in env) {
+            if (hasOwnProperty(process.env, key)) {
+                continue;
+            }
+
+            set(key, env[key]);
+        }
+
+        return env;
+    } catch (error) {
+        return {};
+    }
+}
+
+function get(key?: string): string {
+    const value = process.env[key];
+
+    if (isUndefined(value)) {
+        return "";
+    }
+
+    return value;
+}
+
+function set(key: string, value: string): void {
+    process.env[key] = value;
+}
+
+const envAgent: EnvManipulator = {
+    parse: parse,
+    configure: configure,
+    get: get,
+    set: set
+};
+
+export default envAgent;
