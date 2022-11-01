db0c4db877fa542d4e77a74fc324398de2328785
Michael <me@miikey.pw>
2022-11-01 17:23:50 +1100

    test: included unit tests for configuration options

--- test/env-agent.test.ts
+++ test/env-agent.test.ts
@@ -3,7 +3,7 @@ import path from "path";
 import chai from "chai";
 
 import envAgent from "../lib/env-agent";
-import { hasOwnProperty } from "../lib/utils";
+import { hasOwnProperty, shallowMerge } from "../lib/utils";
 
 const DOTENV_FILENAME = envAgent.DOTENV_FILENAME;
 
@@ -37,6 +37,94 @@ describe("envAgent", () => {
         fs.unlinkSync(DOTENV_FILENAME);
     });
 
+    afterEach(() => {
+        for (const key in parsedEnvironmentVariables) {
+            if (hasOwnProperty(process.env, key)) {
+                delete process.env[key];
+            }
+        }
+    });
+
+    it("should accept a configuration object", () => {
+        const agent = envAgent.create();
+
+        const options = {
+            silent: true,
+            strict: true,
+            debug: false
+        };
+
+        agent.configure(options);
+
+        chai.expect(agent.options).to.deep.equal(
+            shallowMerge(options, agent.options)
+        );
+    });
+
+    describe("configuration options", () => {
+        it("should throw an error when `silent` is false (silent)", () => {
+            const agent = envAgent.create();
+
+            const options = {
+                silent: false,
+                path: "fake/path"
+            };
+
+            chai.expect(() => agent.configure(options)).to.throw();
+        });
+
+        it("should not throw an error when `silent` is true (silent)", () => {
+            const agent = envAgent.create();
+
+            const options = {
+                silent: true,
+                path: "fake/path"
+            };
+
+            chai.expect(() => agent.configure(options)).to.not.throw();
+        });
+
+        it("should set the variable when the value is not defined (strict)", () => {
+            const agent = envAgent.create();
+
+            const options = {
+                silent: true,
+                strict: false
+            };
+
+            agent.configure(options);
+
+            chai.expect(hasOwnProperty(process.env, "MISSING_VAR")).to.be.true;
+            chai.expect(agent.get("MISSING_VAR")).to.equal("");
+        });
+
+        it("should not set the variable when the value is not defined (strict)", () => {
+            const agent = envAgent.create();
+
+            const options = {
+                strict: true
+            };
+
+            agent.configure(options);
+
+            chai.expect(hasOwnProperty(process.env, "MISSING_VAR")).to.be.false;
+            chai.expect(agent.get("MISSING_VAR")).to.equal("");
+        });
+
+        it("should allow a custom path to the .env file", () => {
+            const agent = envAgent.create();
+
+            const options = {
+                silent: true,
+                path: "fake/path"
+            };
+
+            agent.configure(options);
+
+            chai.expect(agent.options.path).to.equal(options.path);
+        });
+    });
+
     it("should parse a buffer into an object", () => {
         const envPath = path.resolve(process.cwd(), DOTENV_FILENAME);
         const file = fs.readFileSync(envPath);
@@ -60,6 +148,8 @@ describe("envAgent", () => {
     });
 
     it("should get an environment variable", () => {
+        envAgent.configure();
+
         const port = envAgent.get("PORT");
         const nodeEnv = envAgent.get("NODE_ENV");
 
@@ -71,5 +161,6 @@ describe("envAgent", () => {
         envAgent.set("TEST_VAR", "test");
 
         chai.expect(envAgent.get("TEST_VAR")).to.equal("test");
+        chai.expect(hasOwnProperty(process.env, "TEST_VAR")).to.be.true;
     });
 });
